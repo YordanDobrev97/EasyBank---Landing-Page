ad04d2909e63fe099d73cea1cf00014d5f3f0dcf
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-07-17 01:40:27 +0300

    Fix js image location

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const image = document.querySelector('.fa-bars');
 hamburger.addEventListener('click', () => {
     if (!nav.classList.contains('show')) {
         nav.classList.toggle('show');
-        image.src = '../images/icon-close.svg';
+        image.src = './images/icon-close.svg';
     } else {
         nav.classList.remove('show');
-        image.src = '../images/icon-hamburger.svg';
+        image.src = './images/icon-hamburger.svg';
     }
 })
